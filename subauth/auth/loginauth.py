6b7b595dff1ffcf32044acdbb4b4103101bebd6a
Marcus R. Breese <marcus.breese@ucsf.edu>
2017-03-26 15:17:54 -0700

    handle pexpect exception

--- subauth/auth/loginauth.py
+++ subauth/auth/loginauth.py
@@ -5,11 +5,13 @@ def auth(username, password):
     clean_username = re.sub('[^a-zA-Z0-9]', '_', username)
     cmd = [ 'su', '--login', '-c /bin/true', clean_username ]
 
-    p = pexpect.spawn(' '.join(cmd), timeout=60)
-    p.expect('.*assword.*')
-    p.sendline(password)
-    p.expect(pexpect.EOF)
-    p.close()
+    try:
+        p = pexpect.spawn(' '.join(cmd), timeout=60)
+        p.expect('.*assword.*')
+        p.sendline(password)
+        p.expect(pexpect.EOF)
+        p.close()
 
-    return p.exitstatus == 0    
-    
+        return p.exitstatus == 0    
+    except:
+        return False
